6da1f829e51210bde971aa2b7087127494eb36a3
lloydice <lloydice@gmail.com>
2023-09-20 16:10:03 -0600

    first-program

---
+++ Starting/my-first-program.py
@@ -0,0 +1,14 @@
+#Desglosemos el código print('¡Hola Mundo!') y expliquémoslo en detalle:
+#   1. ¿Qué haces print? Print es una función incorporada en Python que te permite mostrar la salida en la consola o terminal. 
+#       Toma uno o más argumentos y los imprime en la salida estándar.
+#   2. ¡Hola Mundo! Es una cadena literal entre comillas simples. 
+#       Una cadena es una secuencia de caracteres, y en este caso, contiene el texto "¡Hola Mundo!". 
+#       La cadena es el argumento que se pasa a la función 'print'.
+#   Cuando ejecutas el código print('¡Hola Mundo!'), ocurre lo siguiente:
+#       1. La función 'print' es llamada con el argumento '¡Hola Mundo!'.
+#       2. La función 'print' toma el argumento y lo muestra como salida en la consola o terminal.
+#       3. En este caso, la salida será: ¡Hola Mundo!
+#   La función 'print' es una herramienta útil para mostrar información mientras se ejecuta un programa.
+#   Se puede utilizar para imprimir variables, mensajes o cualquier otro dato que quieras ver durante la ejecución de tu código.
+
+print('Hello World!')
